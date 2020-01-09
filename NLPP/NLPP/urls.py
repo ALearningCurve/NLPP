78f1ca2fa9eea25b59bb35324f78a613f82dc8e8
ALearningCurve <21wwalling-sotolongo@psharvard.org>
2020-01-09 13:23:05 -0500

    fix the navbar

--- NLPP/NLPP/urls.py
+++ NLPP/NLPP/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import handler404
 from django.conf import settings
 from . import views
 
@@ -25,6 +24,7 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
     path('accounts/', include('accounts.urls', namespace="accounts")),
     path('groups/', include('groups.urls', namespace="groups")),
+    path('posts/', include("posts.urls", namespace="posts")),
 ]
 
 if settings.DEBUG:
