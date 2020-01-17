425b4f245dc2fb3429d2e58c99e5df627e23e05e
ALearningCurve <21wwalling-sotolongo@psharvard.org>
2020-01-17 10:07:30 -0500

    add api

---
+++ NLPP/static/NLPP/js/yandex_api.js
@@ -0,0 +1,8 @@
+
+/**
+https://translate.yandex.net/api/v1.5/tr.json/translate
+ ?key=trnsl.1.1.20200117T014254Z.b5ec263d36a25c07.698d2465df9fd59677fe87985d82aa6fef88f8af
+ &text=hola [string of text right here]
+ &lang=es-en [language codes, no code here is auto detect, if only one code then auto correct]
+
+*/
