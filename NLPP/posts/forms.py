d95e0e75914f74ddee6d8add7ce419f2d849151d
ALearningCurve <21wwalling-sotolongo@psharvard.org>
2020-01-23 08:45:39 -0500

    Update Templates + JS fix

--- NLPP/posts/forms.py
+++ NLPP/posts/forms.py
@@ -9,12 +9,3 @@ class PostForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         pass
-
-        # user = kwargs.pop("user", None)
-        # super().__init__(*args, **kwargs)
-        # if user is not None:
-        #     self.fields["group"].queryset = (
-        #         models.Group.objects.filter(
-        #             pk__in=user.groups.values_list("group__pk")
-        #         )
-        #     )
